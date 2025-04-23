bf27b23fc3e8f5236961796be37a8de580e49f2b
khianaim <khiana.mannix@gmail.com>
2025-04-23 16:30:20 -0400

    Add files via upload

---
+++ src/components/DisplayTechIcons.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useState } from 'react';
+import { cn, getTechLogos } from '@/lib/utils';
+import Image from "next/image";
+
+const DisplayTechIcons = ({ techStack }: TechIconProps) => {
+  const [techIcons, setTechIcons] = useState<{ tech: string, url: string }[]>([]);
+
+  useEffect(() => {
+    const fetchTechIcons = async () => {
+      const icons = await getTechLogos(techStack);
+      setTechIcons(icons);
+    };
+
+    fetchTechIcons();
+  }, [techStack]); // Run when techStack changes
+
+  return (
+    <div className="flex flex-row">
+      {techIcons.slice(0, 3).map(({ tech, url }, index) => (
+        <div key={tech}   className={cn(
+            "relative group bg-dark-300 rounded-full p-2 flex flex-center",
+        index >= 1 && '-ml-3')}>
+          <span className="tech-tooltip">{tech}</span>
+          <Image src={url} alt={tech} width={100} height={100} className="size-5" />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default DisplayTechIcons;
