27ef23c9f11a65bda2030ae36e20c6f9a7d397a5
khianaim <khiana.mannix@gmail.com>
2025-04-23 14:46:15 -0400

    Update client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { getApp, getApps, initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyC0xcmTRSQ6PH7_eq_zbNUgI8y_dNhOG0Q",
+  authDomain: "byte-prep.firebaseapp.com",
+  projectId: "byte-prep",
+  storageBucket: "byte-prep.firebasestorage.app",
+  messagingSenderId: "491174589602",
+  appId: "1:491174589602:web:186432e1c2e85c79b053fb",
+  measurementId: "G-S8T4DXYXR6"
 };
 
 // Initialize Firebase
