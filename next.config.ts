1983919078ec9ea58fba886ad23492bda0626e5e
khianaim <khiana.mannix@gmail.com>
2025-04-23 16:23:05 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-import path from 'path';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
@@ -7,15 +6,8 @@ const nextConfig: NextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      '@': path.resolve(__dirname, 'src'),
-      '@firebase': path.resolve(__dirname, 'firebase'),
-    };
-    return config;
-  },
+  }
 };
 
 export default nextConfig;
+
