e4935665fa2bdb4236c38f5de899b0d186431716
khianaim <khiana.mannix@gmail.com>
2025-04-23 15:35:07 -0400

    Add files via upload

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Agent from "@/components/agent";
+import { getCurrentUser } from '@/lib/actions/auth.action';
+
+const page = async () => {
+    const user = await getCurrentUser();
+
+  return (
+    <>
+    <h3>Generated Practice Interview</h3>
+<section className='mt-10'>
+        <Agent 
+        userName={user?.name} 
+        userId={user?.id} 
+        profileImage={user?.profileURL}
+        type="generate" />
+        
+        </section>
+    </>
+  )
+}
+
+export default page
