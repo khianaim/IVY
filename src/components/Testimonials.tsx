d3ca18ea7db859420d4d6126661a421f2b078ecb
khianaim <khiana.mannix@gmail.com>
2025-04-23 11:47:56 -0400

    Add files via upload

---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import * as AvatarPrimitive from "@radix-ui/react-avatar";
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+const Avatar = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Root
+    ref={ref}
+    className={cn("relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full", className)}
+    {...props}
+  />
+));
+Avatar.displayName = AvatarPrimitive.Root.displayName;
+
+const AvatarImage = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Image>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Image
+    ref={ref}
+    className={cn("aspect-square h-full w-full", className)}
+    {...props}
+  />
+));
+AvatarImage.displayName = AvatarPrimitive.Image.displayName;
+
+const AvatarFallback = React.forwardRef<
+  React.ElementRef<typeof AvatarPrimitive.Fallback>,
+  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Fallback>
+>(({ className, ...props }, ref) => (
+  <AvatarPrimitive.Fallback
+    ref={ref}
+    className={cn(
+      "flex h-full w-full items-center justify-center rounded-[inherit] bg-secondary text-xs",
+      className,
+    )}
+    {...props}
+  />
+));
+AvatarFallback.displayName = AvatarPrimitive.Fallback.displayName;
+
+export { Avatar, AvatarFallback, AvatarImage };
+
+
+function Testimonials() {
+  return (
+    <div className="flex items-center rounded-full border border-border bg-background p-1 shadow shadow-black/5">
+      <div className="flex -space-x-1.5">
+        <img
+          className="rounded-full ring-1 ring-background"
+          src="https://originui.com/avatar-80-03.jpg"
+          width={20}
+          height={20}
+          alt="Avatar 01"
+        />
+        <img
+          className="rounded-full ring-1 ring-background"
+          src="https://originui.com/avatar-80-04.jpg"
+          width={20}
+          height={20}
+          alt="Avatar 02"
+        />
+        <img
+          className="rounded-full ring-1 ring-background"
+          src="https://originui.com/avatar-80-05.jpg"
+          width={20}
+          height={20}
+          alt="Avatar 03"
+        />
+        <img
+          className="rounded-full ring-1 ring-background"
+          src="https://originui.com/avatar-80-06.jpg"
+          width={20}
+          height={20}
+          alt="Avatar 04"
+        />
+      </div>
+      <p className="px-2 text-xs text-muted-foreground">
+        Trusted <strong className="font-medium text-foreground"> by 5K+</strong> people who landed a job in the first round.
+      </p>
+    </div>
+  );
+}
+
+export { Testimonials };
